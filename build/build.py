53120be3a0af82ababffe616a061f70e2b5783ec
gjr-rj <gjr.doc@gmail.com>
2023-03-28 01:37:21 -0300

    Create build Options

--- build/build.py
+++ build/build.py
@@ -1,19 +1,60 @@
 import subprocess
+import argparse
 
 def doc_generate():
     subprocess.run(["doxygen.exe", "../Doxyfile"])
 
-def binary_build():
+def build_release():
     subprocess.run(["cmake", "../CMakeLists.txt"])
     subprocess.run(["cmake", "--build", ".", "--config", "release"])
+
+def build_debug():
+    subprocess.run(["cmake", "../CMakeLists.txt"])
+    subprocess.run(["cmake", "--build", ".", "--config", "debug"])
 
 def exec_smoke():
     subprocess.run(["../bin/Release/bignumlibQA.exe"])
 
 def main():
-    doc_generate()
-    binary_build()
-    exec_smoke()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--debug',
+                    action='store_true',
+                    dest='debug',
+                    help='Build debug mode'
+                    )
+
+    parser.add_argument('-r', '--release',
+                    action='store_true',
+                    dest='release',
+                    help='Build release mode'
+                    )
+
+    parser.add_argument('-s', '--smoke',
+                    action='store_true',
+                    dest='smoke',
+                    help='Run automated tests'
+                    )
+
+    parser.add_argument('-D', '--doc',
+                    action='store_true',
+                    dest='doc',
+                    help='Generates documentation'
+                    )
+
+    args = parser.parse_args()
+
+    if args.doc:
+        doc_generate()
+
+    if args.debug:
+        build_debug()
+
+    if args.release:
+        build_release()
+
+    if args.smoke:
+        exec_smoke()
+        
 
 if __name__=="__main__":
     main()
